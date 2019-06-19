c62d8144e5ac36a1cfeed2b1c85c783c5e25d5c5
Nyannazz <kantemir.imam@gmail.com>
2019-06-19 13:42:56 +0200

    fixes

---
+++ public/precache-manifest.5d1db01f73696090d0ef383aeda44a5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52393270c536115caa5288b089057f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feb69934f9bbeb014bf6",
+    "url": "/static/css/main.c6559e9e.chunk.css"
+  },
+  {
+    "revision": "59abea30a5c4e3f855e7",
+    "url": "/static/js/2.def13f26.chunk.js"
+  },
+  {
+    "revision": "feb69934f9bbeb014bf6",
+    "url": "/static/js/main.a17b7094.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- routes/web.php
+++ routes/web.php
@@ -20,40 +20,39 @@ Route::post('api/login', 'UserController@authenticate');
 
 
 Route::group(['middleware' => ['jwt.verify']], function() {
-    Route::get('api/user', 'UserController@getAuthenticatedUser'); 
-    Route::get('api/favorite/{postId}','PostsController@toggleFavorite')->where('id', '[0-9]+');
-    Route::get('api/logged/posts/{postId}','PostsController@getPost');
-    Route::get('api/logged/user','PostsController@getByUser');
-    Route::post('api/logged/posts','PostsController@store');
-    Route::get('api/logged/favorites','PostsController@getFavorites');
+    Route::get('/user', 'UserController@getAuthenticatedUser'); 
+    Route::get('/favorite/{postId}','PostsController@toggleFavorite')->where('id', '[0-9]+');
+    Route::get('/logged/posts/{postId}','PostsController@getPost');
+    Route::get('/logged/user','PostsController@getByUser');
+    Route::post('/logged/posts','PostsController@store');
+    Route::get('/logged/favorites','PostsController@getFavorites');
 
 });
 
-Route::get('/api/controller', "PagesController@index");
+Route::get('/controller', "PagesController@index");
 
-Route::post('/api/signup','RegistrationController@store');
-Route::get('/api/login', function(){
+Route::post('/signup','RegistrationController@store');
+Route::get('/login', function(){
     return "you are not logged in";
 })->name('login');
 
 /* Route::post('/login', 'SessionsController@store');
 Route::get('/logout', 'SessionsController@destroy'); */
 
-Route::get('/api/posts','PostsController@index');
-Route::get('/api/posts/new','PostsController@getNew');
-Route::get('/api/posts/popular','PostsController@getPopular');
-Route::get('/api/posts/tag/{tagname}','PostsController@getByTag');
-Route::get('/api/posts/search_strict/{tagname}','PostsController@searchStrict');
-Route::get('/api/posts/search/{tagname}','PostsController@search');
-Route::get('/api/posts/{id}','PostsController@show')->where('id', '[0-9]+');
+Route::get('/posts','PostsController@index');
+Route::get('/posts/new','PostsController@getNew');
+Route::get('/posts/popular','PostsController@getPopular');
+Route::get('/posts/tag/{tagname}','PostsController@getByTag');
+Route::get('/posts/search_strict/{tagname}','PostsController@searchStrict');
+Route::get('/posts/search/{tagname}','PostsController@search');
+Route::get('/posts/{id}','PostsController@show')->where('id', '[0-9]+');
 
-Route::post('/api/posts','PostsController@store');
+Route::post('/posts','PostsController@store');
 
 
 /* Route::resource('posts','PostsController'); */
-Route::resource('/api/comments','CommentsController');
+Route::resource('/comments','CommentsController');
 
 
-Route::get('/', 'HomeController@index')->name('home');
 
 
